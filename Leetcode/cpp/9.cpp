9d9b9cf84ebfaae7e1be05ec888a93bbdb19915a
James-S-007 <jspringer8@gatech.edu>
2022-02-07 11:54:19 -0500

    Namespace fix

--- Leetcode/cpp/9.cpp
+++ Leetcode/cpp/9.cpp
@@ -5,7 +5,7 @@ bool isPalindrome(int x) {
         return false;
     }
     
-    std::string x_str = to_string(x);
+    std::string x_str = std::to_string(x);
     for (int i = 0; i < x_str.length() / 2; i++) {
         if (x_str[i] != x_str[x_str.length()-i-1]) {
             return false;
